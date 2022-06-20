9d4cf34c8d1c9969ff6c028cc8a545023107b898
Archit Jain <arcyjain2002@gmail.com>
2022-06-20 19:56:29 +0530

    overloading of constructors

---
+++ 2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Base //  we overload the constructor with certain parameters
+{
+
+public:
+    Base() // default constructor is called
+    {
+
+        cout << "default constructor is called" << endl;
+    }
+
+    Base(int x) // constructor with 1 parameter
+    {
+
+        cout << "constructor with 1 parameter is called with value " << x << endl;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Base b1;      // no parameters passed
+    Base b2(100); // parameter is passed as 100
+    return 0;
+}
