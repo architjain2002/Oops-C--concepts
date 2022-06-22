9a4d1056b999e777f73fd387b6d9a68f5305d69a
Archit Jain <arcyjain2002@gmail.com>
2022-06-22 19:56:28 +0530

    pure virtual functions

--- 14.Pure_virtual_functions.cpp
+++ 14.Pure_virtual_functions.cpp
@@ -1,3 +1,16 @@
+/*******************************************************
+ * Copyright (C) 2022 Archit Jain arcyjain2002@gmail.com
+ *
+ * This file is part of my work.
+ *
+ * Dont worry you can copy and/or distribute without the express
+ * permission of Archit Jain lol.
+ *******************************************************/
+// ────█▀█▄▄▄▄─────██▄
+// ────█▀▄▄▄▄█─────█▀▀█
+// ─▄▄▄█─────█──▄▄▄█
+// ██▀▄█─▄██▀█─███▀█
+// ─▀▀▀──▀█▄█▀─▀█▄█▀
 
 #include <iostream>
 using namespace std;
