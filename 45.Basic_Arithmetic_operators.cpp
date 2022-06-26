68ec7e1b9b49a40050b4dc0163b9f21137c3e970
Archit Jain <arcyjain2002@gmail.com>
2022-06-26 22:40:39 +0530

    Arithmetic operator

---
+++ 45.Basic_Arithmetic_operators.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 11;
+    int b = 5;
+    int c;
+
+    cout << "a + b is :" << a + b << endl; // 11+5
+
+    cout << "a - b is :" << a - b << endl; // 11-5
+
+    cout << "a * b is :" << a * b << endl; // 11*5
+
+    cout << "a / b is :" << a / b << endl; // 11/5
+
+    cout << "a % b is :" << a % b << endl; // 11%5
+
+    cout << "a++ is :" << a++ << endl; // 11++
+
+    cout << "a-- is :" << a-- << endl; // 12--
+
+    return 0;
+}
